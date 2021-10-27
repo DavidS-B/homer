19cdf8c3ec3cde89208131b8157e6d5f196e0e2b
DSB <d.sentenac-bossiere@hotmail.fr>
2021-10-27 08:01:57 +0200

    dockerizing app - fix repo test conf

--- lib/homer/repo.ex
+++ lib/homer/repo.ex
@@ -2,4 +2,19 @@ defmodule Homer.Repo do
   use Ecto.Repo,
     otp_app: :homer,
     adapter: Ecto.Adapters.Postgres
+
+    def init(_, config) do
+      config =
+        config
+        |> Keyword.put(:username, System.get_env("PGUSER", "postgres"))
+        |> Keyword.put(:password, System.get_env("PGPASSWORD", "postgres"))
+        |> Keyword.put(:database, System.get_env("PGDATABASE", "postgres"))
+        |> Keyword.put(:hostname, System.get_env("PGHOST", "localhost"))
+        |> Keyword.update(:port, normalize_port(System.get_env("PGPORT")), &normalize_port/1)
+
+      {:ok, config}
+    end
+
+    defp normalize_port(port) when is_binary(port), do: String.to_integer(port)
+    defp normalize_port(port), do: port
 end
